c1781a989328657dcdc2a2eae467df6a86aa346a
CodeSandbox <support@codesandbox.io>
2024-01-15 06:41:05 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello CodeSandbox");
+"use strict";
+function add(n1, n2) {
+    return n1 + n2;
+}
+const number1 = 30;
+const number2 = 10;
+const result = add(number1, number2);
+console.log(result);
